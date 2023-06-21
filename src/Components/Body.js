73be45dad82cefc428a87df9ab02847d72932be5
Shahid Raza <razashahid532@gmail.com>
2023-06-21 18:19:45 +0530

    Added live API

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,8 +1,7 @@
-
 import RestaurantCard from "./RestaurantCard";
 import Data from "../../src/utils/data/swiggy.json";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { SWIGGY_URL } from "../utils/common/constants";
 let resList = Data.data.cards;
 
 // Body Component
@@ -10,7 +9,21 @@ const Body = () => {
   // Hooks
   const [restaurantList, setRestaurantList] = useState(resList);
   const [filterActive, setFilterActive] = useState(false);
+  // useEffect Hooks
+  useEffect(() => {
+    fetchSwiggy;
+  }, []);
 
+  const fetchSwiggy = async () => {
+    const data = await fetch(SWIGGY_URL);
+    const json = await data.json();
+    console.log(json.data);
+    if (json.data.cards[2] !== undefined) {
+      setRestaurantList(json.data.cards[2].data.data.cards);
+    }
+  };
+
+  fetchSwiggy();
   const handleFilterClick = () => {
     let filteredList = [];
     if (filterActive) {
--- src/utils/common/constants.js
+++ src/utils/common/constants.js
@@ -3,4 +3,8 @@ export const HEADER_LOGO =
 export const CARDS_IMG_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
+export const SWIGGY_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&page_type=DESKTOP_WEB_LISTING";
 // export default { HEADER_LOGO, CARDS_IMG_URL };
+
+export const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
